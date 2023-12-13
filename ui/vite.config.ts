a355281f91e0e8027c266f36d21e175e12c4231b
yanle <yanle@talkweb.com.cn>
2023-12-13 20:13:15 +0800

    chore: modify vite configuration to solve static resource caching problem

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -8,13 +8,6 @@ export default defineConfig({
     "process.env.NODE_ENV": '"production"',
   },
   build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: "entry.js",
-      name: "WorkspaceManager",
-      // the proper extensions will be added
-      fileName: "workspace-manager",
-    },
     watch: {
       include: ["src/**"],
     },
@@ -24,6 +17,9 @@ export default defineConfig({
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ["/scripts/app.js"],
+      input: {
+        input: "entry.js"
+      },
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
@@ -32,6 +28,8 @@ export default defineConfig({
           Litegraph: "LiteGraph",
         },
         dir: "../dist",
+        assetFileNames: "[name]-[hash][extname]",
+        entryFileNames: "workspace-manager-[hash].js"
       },
     },
   },
