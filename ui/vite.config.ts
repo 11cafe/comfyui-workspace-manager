d4779a733e0db952291cf5139127ca37b8573043
Weixuan Fu <weixuanf@outlook.com>
2023-12-14 19:20:54 +0800

    revert vite config before thorogh testing

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
     "process.env.NODE_ENV": '"production"',
   },
   build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: "entry.js",
+      name: "WorkspaceManager",
+      // the proper extensions will be added
+      fileName: "workspace-manager",
+    },
     watch: {
       include: ["src/**"],
     },
@@ -17,9 +24,6 @@ export default defineConfig({
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ["/scripts/app.js"],
-      input: {
-        input: "entry.js"
-      },
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
@@ -28,8 +32,6 @@ export default defineConfig({
           Litegraph: "LiteGraph",
         },
         dir: "../dist",
-        assetFileNames: "[name]-[hash][extname]",
-        entryFileNames: "workspace-manager-[hash].js"
       },
     },
   },
