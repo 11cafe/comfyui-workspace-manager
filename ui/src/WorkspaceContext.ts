1ce5226bbd9ca2c5473a38fb21476828d2a68fd7
yanle <421361608@qq.com>
2023-12-22 03:00:17 +0800

    merge dev

--- ui/src/WorkspaceContext.ts
+++ ui/src/WorkspaceContext.ts
@@ -1,11 +1,9 @@
 import { createContext } from "react";
 import { Folder, Workflow } from "./WorkspaceDB";
-import { EWorkspacePosition } from "./RecentFilesDrawer/types";
 
 export const WorkspaceContext = createContext<{
   curFlowID: string | null;
   onDuplicateWorkflow?: (flowID: string) => void;
-  onPositionChange?: (position: EWorkspacePosition) => void;
   loadWorkflowID: (id: string) => void;
 }>({
   curFlowID: null,
