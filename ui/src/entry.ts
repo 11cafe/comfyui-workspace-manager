726e1262ca53584d49bd136a9050cfc7ff57d1c0
Weixuan Fu <weixuanf@outlook.com>
2024-01-10 11:30:02 +0800

    load after canvas load

--- ui/src/entry.ts
+++ ui/src/entry.ts
@@ -1,5 +1,36 @@
-setTimeout(() => {
-  import("./main.tsx").then(({ setupReact }) => {
-    setupReact();
-  });
-}, 1);
+// setTimeout(() => {
+//   import("./main.tsx").then(({ setupReact }) => {
+//     setupReact();
+//   });
+// }, 1);
+
+// Select the node that will be observed for mutations
+const targetNode = document.body; // or any other parent element you want to observe
+
+// Options for the observer (which mutations to observe)
+const config = { childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function (mutationsList, observer) {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeName === "CANVAS") {
+          console.log("Canvas element added to the DOM:", node);
+          import("./main.tsx").then(({ setupReact }) => {
+            setupReact();
+          });
+        }
+      });
+    }
+  }
+};
+
+// Create an instance of the MutationObserver
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Remember to disconnect the observer when it's no longer needed
+// observer.disconnect();
