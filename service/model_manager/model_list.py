1e92a02bc28055f49315ed50fd3361ddaeac01da
Astray_ <166081994@qq.com>
2024-01-15 17:40:21 +0800

    change to folder_path

--- service/model_manager/model_list.py
+++ service/model_manager/model_list.py
@@ -17,30 +17,28 @@ def get_model_list(request):
     file_list = []
     folders = [folder for folder in os.listdir(model_root_dir) if os.path.isdir(os.path.join(model_root_dir, folder))]
     for folder in folders:
+        if (folder == "configs"):
+            continue
         model_dir = os.path.join(model_root_dir, folder)
-        supported_pt_extensions = set(['.ckpt', '.pt', '.bin', '.pth', '.safetensors'])
-        for root, dirs, files in os.walk(model_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                parent_folder = os.path.basename(root)
-                if os.path.splitext(file_path)[1] in supported_pt_extensions:
-                    model = os.path.splitext(file)[0]
-                    extentsion = os.path.splitext(file)[1]
+       
+        files = folder_paths.get_filename_list(folder)
+        for file in files:
 
-                    # check if the file hash is already calculated
-                    if (file_path in file_hash_dict):
-                        file_hash = file_hash_dict[file_path]
-                    else:
-                        # sha256 hash of the file
-                        sha256_hash = hashlib.sha256()
-                        with open(file_path,"rb") as f:
-                            # Read and update hash string value in blocks of 4K
-                            for byte_block in iter(lambda: f.read(4096),b""):
-                                sha256_hash.update(byte_block)
-                            file_hash = sha256_hash.hexdigest()
-                            file_hash_dict[file_path] = file_hash
-                    
-                    file_list.append({"model_name": model, "model_type": parent_folder, "model_path": file_path, "model_extension": extentsion, "file_hash": file_hash})
+            file_path = os.path.join(model_dir, file)
+            # check if the file hash is already calculated
+            if (file_path in file_hash_dict):
+                file_hash = file_hash_dict[file_path]
+            else:
+                # sha256 hash of the file
+                sha256_hash = hashlib.sha256()
+                with open(file_path,"rb") as f:
+                    # Read and update hash string value in blocks of 4K
+                    for byte_block in iter(lambda: f.read(4096),b""):
+                        sha256_hash.update(byte_block)
+                    file_hash = sha256_hash.hexdigest()
+                    file_hash_dict[file_path] = file_hash
+            model_name = os.path.splitext(file)[0]
+            file_list.append({"model_name": model_name, "model_type": folder, "model_extension": os.path.splitext(file)[1], "file_hash": file_hash})
     return web.json_response(file_list, content_type='application/json')
 
   
