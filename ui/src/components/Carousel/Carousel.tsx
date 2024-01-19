bab82ff1fe7943c44459662f9bc074ec7fe184b0
Weixuan Fu <weixuanf@outlook.com>
2024-01-19 19:25:57 +0800

    fixed ui style

--- ui/src/components/Carousel/Carousel.tsx
+++ ui/src/components/Carousel/Carousel.tsx
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react'
-import {Button, Flex, Image} from '@chakra-ui/react'
-import {AnimatePresence, motion} from 'framer-motion'
-import {isImageFormat} from '../../utils.tsx'
+import React, { useEffect, useState } from "react";
+import { Button, Flex, IconButton, Image } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { isImageFormat } from "../../utils.tsx";
+import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 
 type Media = {
   id: string;
@@ -11,22 +12,26 @@ type Media = {
 interface CarouselProps {
   media: Media[];
   currentNum?: number;
-  setMediaAct?: (media: Media) => void
+  setMediaAct?: (media: Media) => void;
 }
 
-const Carousel: React.FC<CarouselProps> = ({ setMediaAct, media, currentNum }) => {
+const Carousel: React.FC<CarouselProps> = ({
+  setMediaAct,
+  media,
+  currentNum,
+}) => {
   const [current, setCurrent] = useState(0);
   useEffect(() => {
     if (currentNum !== undefined) {
-      setCurrent(currentNum)
+      setCurrent(currentNum);
     }
-  }, [currentNum])
+  }, [currentNum]);
 
   const paginate = (newDirection: number) => {
-    const cur = (current + newDirection + media.length) % media.length
+    const cur = (current + newDirection + media.length) % media.length;
     setCurrent(cur);
     if (setMediaAct) {
-      setMediaAct(media[cur])
+      setMediaAct(media[cur]);
     }
   };
 
@@ -74,47 +79,60 @@ const Carousel: React.FC<CarouselProps> = ({ setMediaAct, media, currentNum }) =
             }}
             style={{
               position: "absolute",
-              width: '100%',
-              height: '100%',
+              width: "100%",
+              height: "100%",
               display: index === current ? "block" : "none",
             }}
+            onClick={() => {
+              window.open(item.imageUrl);
+            }}
           >
-            {isImageFormat(item.imageUrl) ? <Image
+            {isImageFormat(item.imageUrl) ? (
+              <Image
                 src={item.imageUrl}
                 alt={`image-${item.id}`}
-                width={'100%'}
-                height={'100%'}
+                width={"100%"}
+                height={"100%"}
                 objectFit="contain"
-            /> : <video
-                style={{objectFit: 'contain'}}
-                width={'100%'}
-                height={'100%'}
+              />
+            ) : (
+              <video
+                style={{ objectFit: "contain" }}
+                width={"100%"}
+                height={"100%"}
                 src={item.imageUrl}
                 loop={true}
                 autoPlay={true}
                 muted={true}
-            >
-              <track kind="captions"/>
-            </video>}
+              >
+                <track kind="captions" />
+              </video>
+            )}
           </motion.div>
         ))}
       </AnimatePresence>
-      <Button
+      <IconButton
+        size={"sm"}
+        color={"white"}
+        bgColor="whiteAlpha.400"
+        aria-label="Previous image"
+        icon={<IconChevronLeft />}
         onClick={() => paginate(-1)}
         position="absolute"
         left="0"
         zIndex="2"
-      >
-        Prev
-      </Button>
-      <Button
+      />
+      <IconButton
+        size={"sm"}
+        color={"white"}
+        bgColor="whiteAlpha.400"
+        aria-label="Next image"
+        icon={<IconChevronRight />}
         onClick={() => paginate(1)}
         position="absolute"
         right="0"
         zIndex="2"
-      >
-        Next
-      </Button>
+      />
     </Flex>
   );
 };
--- ui/src/gallery/components/MetaBoxByType/OneKSampler.tsx
+++ ui/src/gallery/components/MetaBoxByType/OneKSampler.tsx
@@ -1,27 +1,62 @@
-import {clipboard, ifString, MetaBoxTypeCom, MetaData} from '../../utils.ts'
-import {Box, Button, Flex, IconButton, Input, Textarea, VStack} from '@chakra-ui/react'
-import {IconCopy} from '@tabler/icons-react'
+import { clipboard, ifString, MetaBoxTypeCom, MetaData } from "../../utils.ts";
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Input,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import { IconCopy } from "@tabler/icons-react";
 
-export const calcOneKSampler: (metaData: MetaData) => {[key: string]: string} = (metaData: MetaData) => {
-  const {prompt, workflow} = metaData
-  console.log(workflow)
+export const calcOneKSampler: (metaData: MetaData) => {
+  [key: string]: string;
+} = (metaData: MetaData) => {
+  const { prompt, workflow } = metaData;
+  console.log(workflow);
   // KSampler
-  const kSampler = prompt?.[Object.keys(prompt)?.find((v: string) => prompt[v].class_type === 'KSampler') ?? '']
-  const ckptName = ifString(prompt?.[kSampler?.inputs?.['model']?.[0]]?.inputs?.['ckpt_name']) ?? ifString(prompt?.[Object.keys(prompt)?.find((v: string) => prompt[v]?.inputs?.ckpt_name) ?? '']?.inputs?.ckpt_name) ?? ''
-  const positive = prompt?.[kSampler?.inputs?.['positive']?.[0]]?.['inputs']?.['text']
-  const negative = prompt?.[kSampler?.inputs?.['negative']?.[0]]?.['inputs']?.['text'];
-  const commonFields = ['seed', 'sampler_name', 'scheduler', 'steps', 'cfg'].reduce((previousValue, currentValue) => {
+  const kSampler =
+    prompt?.[
+      Object.keys(prompt)?.find(
+        (v: string) => prompt[v].class_type === "KSampler"
+      ) ?? ""
+    ];
+  const ckptName =
+    ifString(
+      prompt?.[kSampler?.inputs?.["model"]?.[0]]?.inputs?.["ckpt_name"]
+    ) ??
+    ifString(
+      prompt?.[
+        Object.keys(prompt)?.find(
+          (v: string) => prompt[v]?.inputs?.ckpt_name
+        ) ?? ""
+      ]?.inputs?.ckpt_name
+    ) ??
+    "";
+  const positive =
+    prompt?.[kSampler?.inputs?.["positive"]?.[0]]?.["inputs"]?.["text"];
+  const negative =
+    prompt?.[kSampler?.inputs?.["negative"]?.[0]]?.["inputs"]?.["text"];
+  const commonFields = [
+    "seed",
+    "sampler_name",
+    "scheduler",
+    "steps",
+    "cfg",
+  ].reduce((previousValue, currentValue) => {
     if (kSampler?.inputs?.[currentValue]) {
       return {
         ...previousValue,
         [currentValue]: kSampler?.inputs?.[currentValue],
-      }
+      };
     }
-    return previousValue
-  }, {})
-  const latent_image = prompt?.[kSampler?.inputs?.['latent_image']?.[0]]?.['inputs'];
-  const width = latent_image?.['width']
-  const height = latent_image?.['height']
+    return previousValue;
+  }, {});
+  const latent_image =
+    prompt?.[kSampler?.inputs?.["latent_image"]?.[0]]?.["inputs"];
+  const width = latent_image?.["width"];
+  const height = latent_image?.["height"];
 
   return {
     ckptName,
@@ -30,23 +65,43 @@ export const calcOneKSampler: (metaData: MetaData) => {[key: string]: string} = 
     ...commonFields,
     width,
     height,
-  }
-}
+  };
+};
 
-export const OneKSampler: MetaBoxTypeCom = ({metaData}) => {
-  const mediaMetaData = calcOneKSampler(metaData)
-  
-  return <VStack spacing={2} align={'stretch'} >
-    <Box>
-      <Button onClick={() => clipboard(JSON.stringify(mediaMetaData))}>Copy all field</Button>
-    </Box>
-    {Object.keys(mediaMetaData ?? {}).map(key => <Flex key={`meta${key}`} gap={2}>
-      <Flex gap={1} alignItems={'center'} flexBasis={'200px'}>
-        {key}
-        <IconButton size={"xm"} onClick={() => clipboard(mediaMetaData?.[key] ?? '')} aria-label={'copy text'} icon={<IconCopy />} variant={'ghost'} />
-      </Flex>
-      {mediaMetaData?.[key]?.length && mediaMetaData?.[key]?.length > 300 ? <Textarea readOnly={true} value={mediaMetaData?.[key]}/> : <Input readOnly={true} value={mediaMetaData?.[key]}/>}
-      {/*<Text overflowY={'auto'} maxH={200} flex={1}>{mediaMetaData?.[key]}</Text>*/}
-    </Flex>)}
-  </VStack>
-}
+export const OneKSampler: MetaBoxTypeCom = ({ metaData }) => {
+  const mediaMetaData = calcOneKSampler(metaData);
+
+  return (
+    <VStack spacing={2} align={"stretch"}>
+      <Box>
+        <Button
+          size={"sm"}
+          onClick={() => clipboard(JSON.stringify(mediaMetaData))}
+        >
+          Copy all
+        </Button>
+      </Box>
+      {Object.keys(mediaMetaData ?? {}).map((key) => (
+        <Flex key={`meta${key}`} gap={2}>
+          <Flex gap={1} alignItems={"center"} flexBasis={"200px"}>
+            {key}
+            <IconButton
+              size={"xm"}
+              onClick={() => clipboard(mediaMetaData?.[key] ?? "")}
+              aria-label={"copy text"}
+              icon={<IconCopy />}
+              variant={"ghost"}
+            />
+          </Flex>
+          {mediaMetaData?.[key]?.length &&
+          mediaMetaData?.[key]?.length > 300 ? (
+            <Textarea readOnly={true} value={mediaMetaData?.[key]} />
+          ) : (
+            <Input readOnly={true} value={mediaMetaData?.[key]} />
+          )}
+          {/*<Text overflowY={'auto'} maxH={200} flex={1}>{mediaMetaData?.[key]}</Text>*/}
+        </Flex>
+      ))}
+    </VStack>
+  );
+};
