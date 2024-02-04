23265c6a9940d9d03b1660591007f74750fb348c
Arslan Ablikim <arslan.ablikim0@gmail.com>
2024-02-04 19:56:18 +0800

    Add thumbnail fetching and update showNsfwModelThumbnail

--- ui/src/settings/ShowNsfwModelThumbnailSettings.tsx
+++ ui/src/settings/ShowNsfwModelThumbnailSettings.tsx
@@ -1,6 +1,26 @@
 import { Checkbox, Stack } from "@chakra-ui/react";
 import { useState, useEffect, ChangeEvent } from "react";
 import { userSettingsTable } from "../db-tables/WorkspaceDB";
+import { indexdb } from "../db-tables/indexdb";
+import { Model } from "../types/dbTypes";
+
+interface ResponsePartial {
+  id: number;
+  modelId: number;
+  name: string;
+  model: {
+    name: string;
+    type: string;
+    nsfw: boolean;
+  };
+  images: {
+    url: string;
+    nsfw: string;
+    width: number;
+    height: number;
+    hash: string;
+  }[];
+}
 
 export default function ShowNsfwModelThumbnailSettings() {
   const [checked, setChecked] = useState(false);
@@ -23,8 +43,38 @@ export default function ShowNsfwModelThumbnailSettings() {
       })
       .then(() => {
         updateShowThumbnail();
+        reFetchThumbnails();
         window.dispatchEvent(new Event("showNsfwModelThumbnail"));
       });
+  };
+
+  const reFetchThumbnails = async () => {
+    const models = await indexdb.models.toArray();
+    for (let i = 0; i < models.length; i += 5) {
+      // fetch 5 at a time, to avoid rate limit
+      const slice = models.slice(i, i + 5);
+      await Promise.all(slice.map(getThumbnail));
+    }
+  };
+  const getThumbnail = async (model: Model) => {
+    try {
+      const url = `https://civitai.com/api/v1/model-versions/by-hash/${model.fileHash}`;
+      const resp = await fetch(url);
+      const json: ResponsePartial = await resp.json();
+      let image_url: string | undefined;
+      if (checked) {
+        image_url = json?.images?.[0]?.url;
+      } else if (!json.model.nsfw) {
+        const sfwImage = json.images.find((i) => !i.nsfw);
+        image_url = sfwImage?.url;
+      }
+
+      indexdb.models.update(model.id, {
+        imageUrl: image_url ?? null,
+      });
+    } catch (e) {
+      /* ignore */
+    }
   };
 
   return (
