ca47764fc34a7254669579aefa26a1eea9b5d4ff
Arslan Ablikim <arslan.ablikim0@gmail.com>
2024-02-19 20:16:43 +0800

    Refactor model_list.py to use is_populating flag instead of populate_started and populate_done flags

--- service/model_manager/model_list.py
+++ service/model_manager/model_list.py
@@ -22,8 +22,7 @@ file_list = []
 file_list_lock = threading.Lock()
 
 # Add a global variable to track if populate_file_hash_dict is done
-populate_started = False
-populate_done = False
+is_populating = False
 
 def save_file_hash(file_path, file_hash):
     global file_hash_dict
@@ -49,7 +48,7 @@ def process_file(folder, file):
     return {"model_name": model_name, "model_type": folder, "model_extension": model_extension, "file_hash": file_hash}
 
 def populate_file_hash_dict():
-    global populate_done, file_list
+    global is_populating, file_list
     local_file_list = []
     folder_list = dict(folder_paths.folder_names_and_paths)
     for folder in folder_list:
@@ -71,7 +70,7 @@ def populate_file_hash_dict():
                 file["file_hash"] = file_hash
                 send_ws('model_list', file_list)  # update the list with the new hash
     # Set populate_done to True when done
-    populate_done = True
+    is_populating = False
 
 def civit_get_model_by_hash(file_hash: str):
     if (file_hash is None):
@@ -91,19 +90,17 @@ def civit_get_model_by_hash(file_hash: str):
         return None
 
 def start_populate_file_hash_dict():
-    global populate_thread, populate_started, populate_done
-    populate_running = populate_started == True and populate_done == False
-    if (populate_running):
+    global populate_thread, is_populating
+    if (is_populating):
         return
-    populate_started = True
+    is_populating = True
     populate_thread = threading.Thread(target=populate_file_hash_dict)
     populate_thread.daemon = True
     populate_thread.start()
 
-start_populate_file_hash_dict()
-
 @server.PromptServer.instance.routes.get("/model_manager/get_model_list")
 def get_model_list(request):
+    start_populate_file_hash_dict()
     with file_list_lock:
         return web.json_response(file_list, content_type='application/json')
     
