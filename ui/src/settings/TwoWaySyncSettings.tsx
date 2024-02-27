59f900dd08017093a09e54dd3df1bd80f6f77e5c
Weixuan Fu <weixuanf@outlook.com>
2024-02-28 00:50:33 +0800

    add two way sync confirmation dialog

--- ui/src/settings/TwoWaySyncSettings.tsx
+++ ui/src/settings/TwoWaySyncSettings.tsx
@@ -1,10 +1,12 @@
-import { Checkbox, Stack, Text } from "@chakra-ui/react";
+import { Button, Checkbox, Stack, Text } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import { userSettingsTable } from "../db-tables/WorkspaceDB";
+import { userSettingsTable, workflowsTable } from "../db-tables/WorkspaceDB";
+import { useDialog } from "../components/AlertDialogProvider";
+import { downloadWorkflowsZip } from "../utils/downloadWorkflowsZip";
 
 export default function TwoWaySyncSettings() {
   const [checked, setChecked] = useState(false);
-
+  const { showDialog } = useDialog();
   const getTwoWaySync = () => {
     userSettingsTable?.getSetting("twoWaySync").then((res) => {
       setChecked(!!res);
@@ -14,6 +16,32 @@ export default function TwoWaySyncSettings() {
   useEffect(() => {
     getTwoWaySync();
   }, []);
+  const onTwoWaySyncChange = async (e: any) => {
+    // setChecked(e.target.checked);
+    console.log("onTwoWaySyncChange", e.target.checked);
+    if (!e.target.checked) {
+      await userSettingsTable?.upsert({ twoWaySync: e.target.checked });
+      getTwoWaySync();
+      return;
+    }
+    const myWorkflowsDir =
+      await userSettingsTable?.getSetting("myWorkflowsDir");
+    showDialog(
+      <EnableTwoWaySyncConfirm
+        myWorkflowsDir={myWorkflowsDir ?? "undefined"}
+      />,
+      [
+        {
+          label: "I have downloaded all my workflows and ready to enable",
+          onClick: async () => {
+            console.log("ready", e.target.checked);
+            await userSettingsTable?.upsert({ twoWaySync: true });
+            getTwoWaySync();
+          },
+        },
+      ],
+    );
+  };
 
   return (
     <Stack>
@@ -22,15 +50,45 @@ export default function TwoWaySyncSettings() {
         your workspace saving directory in your disk and automatically import
         them into your workspace for you
       </Text>
-      <Checkbox
-        isChecked={checked}
-        onChange={async (e) => {
-          await userSettingsTable?.upsert({ twoWaySync: e.target.checked });
-          getTwoWaySync();
-        }}
-      >
+      <Checkbox isChecked={checked} onChange={onTwoWaySyncChange}>
         Enable two way sync (DO NOT USE!! EXPERIMENTAL!)
       </Checkbox>
+    </Stack>
+  );
+}
+
+function EnableTwoWaySyncConfirm({
+  myWorkflowsDir,
+}: {
+  myWorkflowsDir: string;
+}) {
+  return (
+    <Stack gap={3}>
+      <p>Your workflows will be synced to and from path:</p>
+      <p>
+        <b>{myWorkflowsDir}</b>
+      </p>
+      <p>
+        Please make sure this path is valid. You can change this path anytime in
+        Settings {">"} Workspace Save Direcotry
+      </p>
+      <p>
+        <b>Please download all your workflows before enabling two way sync!</b>{" "}
+        So you can manually import some workflows into your my_workflows/
+        directory on your disk in case something unexpected happens.
+      </p>
+      <Button
+        colorScheme="teal"
+        variant={"outline"}
+        width={"fit-content"}
+        size={"sm"}
+        onClick={async () => {
+          const workflows = await workflowsTable?.listAll();
+          downloadWorkflowsZip(workflows ?? []);
+        }}
+      >
+        Download All My Workflows
+      </Button>
     </Stack>
   );
 }
