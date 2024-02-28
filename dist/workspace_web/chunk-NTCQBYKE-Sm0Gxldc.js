633e48b07f56d179c70e90d1aceef7b9becc6747
Weixuan Fu <weixuanf@outlook.com>
2024-02-28 21:47:21 +0800

    fix everywhere drag drop not working

--- dist/workspace_web/chunk-NTCQBYKE-Sm0Gxldc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./App-Wu6A4dRe.js";import{f as o,j as s}from"./input.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"column",ref:r}));e.displayName="VStack";export{e as V};
