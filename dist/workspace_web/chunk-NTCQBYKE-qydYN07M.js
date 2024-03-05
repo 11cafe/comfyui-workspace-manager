b9cdd8153812a2db873b4e4069cec5df5494fbb1
Weixuan Fu <weixuanf@outlook.com>
2024-03-05 22:32:13 +0800

    build dist

--- dist/workspace_web/chunk-NTCQBYKE-qydYN07M.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./App-7ZVFJHq5.js";import{f as o,j as s}from"./input.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"column",ref:r}));e.displayName="VStack";export{e as V};
