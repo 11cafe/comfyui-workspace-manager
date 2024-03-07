07503596753add23a3d874de4eb13f7576bf3168
gubei <>
2024-03-07 11:23:59 +0800

    Workflow dirt check Adjustment. Ignore dirt check inaccuracy after clicking node

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -224,6 +224,35 @@ export default function App() {
     });
   };
 
+  const deepDiffCheck = (oldData: any, newData: any) => {
+    const oldDataNodes = oldData["nodes"]==null ? [] : oldData["nodes"];
+    const newDataNodes = newData["nodes"]==null ? [] : newData["nodes"];
+    oldData["nodes"] = [];
+    newData["nodes"] = [];
+    let equal = JSON.stringify(oldData) === JSON.stringify(newData);
+    if (equal) {
+      if ( oldDataNodes.length == newDataNodes.length ) {
+        const oldDataNodesMap = oldDataNodes.reduce((map:any, obj:any) => {
+          map.set(obj["id"], JSON.stringify(obj));
+          return map;
+        }, new Map());
+        const newDataNodesMap = newDataNodes.reduce((map:any, obj:any) => {
+          map.set(obj["id"], JSON.stringify(obj));
+          return map;
+        }, new Map());
+        for (let [key, value] of oldDataNodesMap) {
+          if (!newDataNodesMap.has(key) || newDataNodesMap.get(key) !== value) {
+            equal = false;
+            break;
+          }
+        }
+      }
+    }
+    oldData["nodes"]= oldDataNodes;
+    newData["nodes"]= newDataNodes;
+    return equal;
+  }
+
   const checkIsDirty = () => {
     if (curFlowID.current == null) return false;
     const curflow = workflowsTable?.curWorkflow;
@@ -236,7 +265,7 @@ export default function App() {
     } catch (e) {
       console.error("error parsing json", e);
     }
-    const equal = JSON.stringify(graphJson) === JSON.stringify(lastSaved);
+    const equal = deepDiffCheck(graphJson, lastSaved);
     return !equal;
   };
 
