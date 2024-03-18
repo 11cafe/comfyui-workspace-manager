12dbec8d79e40a7c394f4ce65c11aac1b9c7d154
Weixuan Fu <weixuanf@outlook.com>
2024-03-18 22:16:19 +0800

    model dependency check and form submit

--- ui/src/spacejson/DownloadSpaceJsonDialog.tsx
+++ ui/src/spacejson/DownloadSpaceJsonDialog.tsx
@@ -1,4 +1,12 @@
-import { useContext, useEffect, useState } from "react";
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { WorkspaceContext } from "../WorkspaceContext";
 import {
   Badge,
@@ -26,25 +34,86 @@ import { app } from "/scripts/app.js";
 import {
   IconExternalLink,
   IconInfoCircle,
-  IconInfoCircleFilled,
-  IconQuestionMark,
   IconRefresh,
 } from "@tabler/icons-react";
-import { getCivitModelDownloadUrl } from "../utils/civitUtils";
+import { getCivitModelPageUrl } from "../utils/civitUtils";
 
 export default function DownloadSpaceJsonDialog() {
   const { route, setRoute } = useContext(WorkspaceContext);
-  const [result, setResult] = useState<DepsResult>();
+  const [deps, setDeps] = useState<DepsResult>();
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
   useEffect(() => {
     console.log("app.graph", app.graph);
     const graph = app.graph.serialize();
     console.log("graph serialize", graph);
-    extractAndFetchFileNames(graph.nodes ?? []).then((result) => {
-      console.log(result);
-      setResult(result);
+    extractAndFetchFileNames(graph.nodes ?? []).then((deps) => {
+      console.log(deps);
+      setDeps(deps);
     });
   }, []);
-  if (!result) {
+  const validateURL = (url: string) => {
+    const pattern = new RegExp(
+      "^(https?:\\/\\/)?" +
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+        "((\\d{1,3}\\.){3}\\d{1,3}))" +
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+        "(\\?[;&a-z\\d%_.~+=-]*)?" +
+        "(\\#[-a-z\\d_]*)?$",
+      "i",
+    );
+    return !!pattern.test(url);
+  };
+  const validateModelDeps = useCallback((modelDeps: ModelFile[]) => {
+    let isValid = true;
+    modelDeps.forEach((model) => {
+      const key = model.filename + model.nodeType;
+      if (!model.downloadUrl) {
+        setErrors((prev) => ({
+          ...prev,
+          [key]: "Invalid URL",
+        }));
+        isValid = false;
+        return;
+      }
+      if (!validateURL(model.downloadUrl)) {
+        setErrors((prev) => ({ ...prev, [key]: "Invalid URL" }));
+        isValid = false;
+        return;
+      }
+      if (!model.fileFolder) {
+        setErrors((prev) => ({
+          ...prev,
+          [key]: "Model file not found, please re-scan models folder",
+        }));
+        isValid = false;
+        return;
+      }
+    });
+    return isValid;
+  }, []);
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const modelDeps =
+      deps?.models.map((model) => {
+        return {
+          ...model,
+          downloadUrl:
+            model.downloadUrl ??
+            formData.get(model.filename + model.nodeType)?.toString() ??
+            null,
+        };
+      }) ?? [];
+    if (!validateModelDeps(modelDeps)) {
+      return;
+    }
+
+    setErrors({});
+  };
+
+  if (!deps) {
     return <div>Preparing...</div>;
   }
 
@@ -67,30 +136,36 @@ export default function DownloadSpaceJsonDialog() {
           </HStack>
           <Text color={"GrayText"}>Export one-click installable workflow</Text>
           <Stack gap={5} mt={5}>
-            <Stack>
-              <Heading size={"sm"}>Models ({result.models.length})</Heading>
-              {result.models.map((modelFile, index) => (
-                <ModelDepsItem modelFile={modelFile} key={index} />
-              ))}
-            </Stack>
-
-            {result.models.length > 0 && (
+            <form onSubmit={handleSubmit}>
               <Stack>
-                <Heading size={"sm"}>Images ({result.images.length})</Heading>
-                {result.images.map((image) => (
-                  <Stack>
-                    <p key={image.filename}>{image.filename}</p>
-                    <Image
-                      width={250}
-                      src={`/workspace/view_media?filename=${image.filename}&isPreview=true&isInput=true`}
-                    />
-                  </Stack>
+                <Heading size={"sm"}>Models ({deps.models.length})</Heading>
+                {deps.models.map((modelFile, index) => (
+                  <ModelDepsItem
+                    modelFile={modelFile}
+                    key={index}
+                    errors={errors}
+                  />
                 ))}
               </Stack>
-            )}
-            <Button width={"fit-content"} colorScheme="teal">
-              Download .space.json
-            </Button>
+
+              {deps.models.length > 0 && (
+                <Stack>
+                  <Heading size={"sm"}>Images ({deps.images.length})</Heading>
+                  {deps.images.map((image) => (
+                    <Stack key={image.filename}>
+                      <p>{image.filename}</p>
+                      <Image
+                        width={250}
+                        src={`/workspace/view_media?filename=${image.filename}&isPreview=true&isInput=true`}
+                      />
+                    </Stack>
+                  ))}
+                </Stack>
+              )}
+              <Button width={"fit-content"} colorScheme="teal" type="submit">
+                Download .space.json
+              </Button>
+            </form>
           </Stack>
         </ModalBody>
       </ModalContent>
@@ -98,8 +173,19 @@ export default function DownloadSpaceJsonDialog() {
   );
 }
 
-function ModelDepsItem({ modelFile }: { modelFile: ModelFile }) {
-  if (!modelFile.models?.length) {
+function ModelDepsItem({
+  modelFile,
+  errors,
+}: {
+  modelFile: ModelFile;
+  errors: Record<string, string>;
+}) {
+  useEffect(() => {
+    console.log("modelFile", modelFile);
+  }, []);
+  const inputKey = modelFile.filename + modelFile.nodeType;
+
+  if (!modelFile.fileFolder) {
     return (
       <Stack>
         <Flex gap={2}>
@@ -108,30 +194,36 @@ function ModelDepsItem({ modelFile }: { modelFile: ModelFile }) {
             Model file not found
           </Tag>
         </Flex>
-        <Button size={"sm"} leftIcon={<IconRefresh />} width={"fit-content"}>
+        <Button
+          size={"sm"}
+          leftIcon={<IconRefresh />}
+          width={"fit-content"}
+          borderColor={"red"}
+          borderWidth={errors[inputKey] != null ? 2 : 0}
+        >
           Fetch model file
         </Button>
       </Stack>
     );
   }
-  const model = modelFile.models[0];
-  const downloadLink = model.civitModelVersionID
-    ? getCivitModelDownloadUrl(model.civitModelVersionID)
-    : model.downloadUrl;
   return (
     <Stack ml={2}>
       <Flex key={modelFile.filename + modelFile.nodeType} gap={2}>
-        {modelFile.models?.map((model, index) => (
-          <Tag
-            height={"fit-content"}
-            colorScheme={index === 0 ? "teal" : undefined}
-          >
-            {model.fileFolder}
-          </Tag>
-        ))}
-        {modelFile.models[0].civitModelVersionID ? (
+        <Tag height={"fit-content"} colorScheme={"teal"}>
+          {modelFile.fileFolder}
+        </Tag>
+
+        {modelFile.downloadUrl ? (
           <HStack>
-            <a href={downloadLink}>{modelFile.filename}</a>
+            <a
+              href={
+                modelFile.civitModelID
+                  ? getCivitModelPageUrl(modelFile.civitModelID)
+                  : modelFile.downloadUrl
+              }
+            >
+              {modelFile.filename}
+            </a>
             <IconExternalLink size={18} />
           </HStack>
         ) : (
@@ -143,10 +235,13 @@ function ModelDepsItem({ modelFile }: { modelFile: ModelFile }) {
               label={"Model download link, supports Huggingface and Civitai"}
             >
               <Input
+                name={inputKey}
                 size={"sm"}
+                isInvalid={errors[inputKey] != null}
                 placeholder={"https://civitai.com/api/download/models/324524"}
               />
             </Tooltip>
+            <Text color={"red.400"}>{errors[inputKey]}</Text>
           </Stack>
         )}
       </Flex>
