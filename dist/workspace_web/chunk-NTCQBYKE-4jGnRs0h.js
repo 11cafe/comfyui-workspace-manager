bfbced53952010a84efdd78f0e8d2ccf2b008480
Weixuan Fu <weixuanf@outlook.com>
2024-03-19 23:15:37 +0800

    fix some isdirty problem, like when clicking chakra menus and it will set canvas as dirty :(

--- dist/workspace_web/chunk-NTCQBYKE-4jGnRs0h.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./App-m2_qh9T8.js";import{f as o,j as s}from"./input.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"column",ref:r}));e.displayName="VStack";export{e as V};
