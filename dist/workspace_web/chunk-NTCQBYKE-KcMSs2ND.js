f566650a60360c9ee446102a76b39cddd9315ba7
Weixuan Fu <weixuanf@outlook.com>
2024-04-17 18:03:33 +0800

    build last pr: update share dialog with resource deps form

---
+++ dist/workspace_web/chunk-NTCQBYKE-KcMSs2ND.js
@@ -0,0 +1,1 @@
+import{g as t}from"./App-YasK6gmY.js";import{f as o,j as s}from"./input.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"column",ref:r}));e.displayName="VStack";export{e as V};
