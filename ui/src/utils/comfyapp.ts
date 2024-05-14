d972cf8e883bb7fc0f5d32cdd97a3e93c512576b
Weixuan Fu <weixuanf@outlook.com>
2024-05-14 18:25:35 +0800

    fix app import to do lazy load

--- ui/src/utils/comfyapp.ts
+++ ui/src/utils/comfyapp.ts
@@ -1,9 +1,13 @@
 // @ts-ignore
-export const app = window.app;
+export let app: any | null = null;
 const api_base = location.pathname.split("/").slice(0, -1).join("/");
 
 export let api: any | null = null;
 
 import(api_base + "/scripts/api.js").then((apiJs) => {
   api = apiJs?.api;
+});
+
+import(api_base + "/scripts/app.js").then((appJs) => {
+  app = appJs?.app;
 });
