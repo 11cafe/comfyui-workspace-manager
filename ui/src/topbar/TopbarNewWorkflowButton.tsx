296a65aab75e4beecc40c389b7e4ba8e4f3a4f9a
Weixuan Fu <weixuanf@outlook.com>
2024-06-21 17:44:19 +0800

    remove + newworkflow button in topbar

--- ui/src/topbar/TopbarNewWorkflowButton.tsx
+++ ui/src/topbar/TopbarNewWorkflowButton.tsx
@@ -1,40 +1,48 @@
 import { Tooltip, Button } from "@chakra-ui/react";
-import { IconDeviceFloppy, IconPlus } from "@tabler/icons-react";
+import { IconDeviceFloppy } from "@tabler/icons-react";
 import { useContext } from "react";
 import { WorkspaceContext } from "../WorkspaceContext";
-import { TOPBAR_BUTTON_HEIGHT } from "../const";
+import { COMFYSPACE_TRACKING_FIELD_NAME, TOPBAR_BUTTON_HEIGHT } from "../const";
 import { app } from "../utils/comfyapp";
 import { workflowsTable } from "../db-tables/WorkspaceDB";
+import { nanoid } from "nanoid";
 
 export default function TopbarNewWorkflowButton() {
-  const { loadNewWorkflow, setCurFlowIDAndName, curFlowID } =
-    useContext(WorkspaceContext);
+  const { setCurFlowIDAndName, curFlowID } = useContext(WorkspaceContext);
   const saveNewWorkflow = async () => {
-    const graph = JSON.stringify(app.graph.serialize());
     const workflowName = prompt("Enter workflow name", "Untitled");
     if (!workflowName) return;
+    const id = nanoid();
+    const graph = app.graph.serialize();
+    if (!graph.extra) {
+      graph.extra = {};
+    }
+    if (!graph.extra[COMFYSPACE_TRACKING_FIELD_NAME]) {
+      graph.extra[COMFYSPACE_TRACKING_FIELD_NAME] = {};
+    }
+    graph.extra[COMFYSPACE_TRACKING_FIELD_NAME].id = id;
     const flow = await workflowsTable?.createFlow({
-      json: graph,
+      id,
+      json: JSON.stringify(graph),
       name: workflowName,
     });
     flow && setCurFlowIDAndName(flow);
   };
+  if (curFlowID) {
+    return null;
+  }
   return (
-    <Tooltip label={curFlowID ? "New workflow" : "Save workflow"}>
+    <Tooltip label={"Save workflow"}>
       <Button
         size={"sm"}
         variant={"outline"}
         colorScheme="teal"
         aria-label="new workflow"
         height={TOPBAR_BUTTON_HEIGHT + "px"}
-        onClick={() => (curFlowID ? loadNewWorkflow() : saveNewWorkflow())}
+        onClick={() => saveNewWorkflow()}
         px={1}
       >
-        {curFlowID ? (
-          <IconPlus size={16} color={"white"} />
-        ) : (
-          <IconDeviceFloppy size={16} color={"white"} />
-        )}
+        <IconDeviceFloppy size={16} color={"white"} />
       </Button>
     </Tooltip>
   );
