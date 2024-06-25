203a3578375962d2d92b54dc0c342162671543a0
Weixuan Fu <weixuanf@outlook.com>
2024-06-25 22:11:21 +0800

    fix workspace manager not showup after comfyui updated

--- entry/entry.js
+++ entry/entry.js
@@ -1,27 +1,6 @@
-const targetNode = document.body;
+//@ts-ignore
+import { api } from "../../scripts/api.js";
 
-// Options for the observer (which mutations to observe)
-const config = { childList: true, subtree: true };
-const api_base = window.location.pathname.split("/").slice(0, -1).join("/");
-
-// @ts-ignore
-const callback = function (mutationsList, observer) {
-  for (const mutation of mutationsList) {
-    if (mutation.type === "childList") {
-      // @ts-ignore
-      mutation.addedNodes.forEach((node) => {
-        if (node.nodeName === "CANVAS") {
-          // @ts-ignore
-          import(api_base + "/workspace_web/input.js");
-          observer.disconnect();
-        }
-      });
-    }
-  }
-};
-
-// Create an instance of the MutationObserver
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+setTimeout(() => {
+  import(api.api_base + "/workspace_web/input.js");
+}, 500);
